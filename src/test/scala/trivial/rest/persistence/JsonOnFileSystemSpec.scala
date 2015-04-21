bb5177206a8d23e81fff4459450c73e6ced5d913
agmenc <chris.agmen-smith@email.com>
2015-04-21 12:05:45 +0100

    Delete files created by tests.

--- src/test/scala/trivial/rest/persistence/JsonOnFileSystemSpec.scala
+++ src/test/scala/trivial/rest/persistence/JsonOnFileSystemSpec.scala
@@ -1,25 +1,30 @@
 package trivial.rest.persistence
 
-import org.scalatest.{MustMatchers, WordSpec}
+import org.scalatest.{BeforeAndAfterAll, MustMatchers, WordSpec}
 
 import scala.reflect.io.Directory
 
-class JsonOnFileSystemSpec extends WordSpec with MustMatchers {
+class JsonOnFileSystemSpec extends WordSpec with MustMatchers with BeforeAndAfterAll {
+  val docRoot = Directory("target/json")
+
   "Meets the PersisterContract terms" in {
 //    new PersisterContract(new JsonOnFileSystem("target/test"))
     fail("train time")
   }
   
   "If a doc root doesn't exist, it is created" in {
-    val docRoot = Directory("target/test/monkeys")
-    docRoot.exists mustBe false
+    val monkeys = Directory(docRoot / "monkeys")
+    monkeys.exists mustBe false
 
-    new JsonOnFileSystem(docRoot).save("foo", "bar")
+    new JsonOnFileSystem(monkeys).save("foo", "bar")
 
-    docRoot.exists mustBe true
+    monkeys.exists mustBe true
   }
   
   "Saving a new resource adds it to the relevant file" in {
-    new JsonOnFileSystem(Directory("target/test/jsonOnFileSystem"))
+    new JsonOnFileSystem(Directory(docRoot / "bob"))
   }
+
+  override protected def beforeAll() = docRoot.deleteRecursively()
+  override protected def afterAll()  = beforeAll()
 }
