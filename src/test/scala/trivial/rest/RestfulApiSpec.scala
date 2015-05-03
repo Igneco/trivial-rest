deaa5e3bb8bca48b52683cd6c50665840c7e8f25
agmenc <chris.agmen-smith@email.com>
2015-05-03 21:18:45 +0100

    Prove the Foo case.

--- src/test/scala/trivial/rest/RestfulApiSpec.scala
+++ src/test/scala/trivial/rest/RestfulApiSpec.scala
@@ -28,41 +28,28 @@ class RestfulApiSpec extends WordSpec with MustMatchers with BeforeAndAfterAll w
     validateJsonResponse(app, "/", """["spaceship","vector"]""")
   }
 
-  implicit def toBigDecimal(str: String): BigDecimal = BigDecimal(str)
-  val someVectors = Map(
-    "24" -> Vector(Some("24"), "79", "0.4"),
-    "2"  -> Vector(Some("2"), "7", "0.3"),
-    "4"  -> Vector(Some("4"), "9", "0.2"),
-    "1"  -> Vector(Some("1"), "8", "0.1")
-  )
-  
-  val someSpaceships = Seq(
-    Spaceship(Some("7"), "Enterprise", 150, someVectors("24")),
-    Spaceship(Some("5"), "Enterprise", 20, someVectors("2")),
-    Spaceship(Some("3"), "Enterprise", 1, someVectors("4")),
-    Spaceship(Some("1"), "Enterprise", 5, someVectors("1"))
-  )
-
-  // TODO - CAS - 03/05/15 - (1) Make this test pass for a bunch of Foo instances
-  // TODO - CAS - 03/05/15 - (2) Add a serialiser for each T that just writes the ID as a String. Make this configurable (some people will want everything.
-  // TODO - CAS - 02/05/15 - (3) Mock the persister in all tests
-  "TODO - Registering a resource type as a GetAll allows bulk download" in {
+  "Registering a resource type as a GetAll allows bulk download" in {
     val persisterMock: Persister = mock[Persister]
+
     val controllerWithRest = new Controller {
       new Rest(this, "/", persisterMock)
-        .resource[Spaceship](GetAll)
+        .resource[Foo](GetAll)
     }
     val app = MockApp(controllerWithRest)
-    
-    (persisterMock.loadAll[Spaceship]  (_: String)(_: Manifest[Spaceship])).expects("spaceship", *).returning(Right(someSpaceships))
 
-    validateJsonResponse(app, "/spaceship.json", """[
-                                                   |{"id": "7", "name": "Enterprise", "personnel": 150, "vector": "24"},
-                                                   |{"id": "5", "name": "Laziness", "personnel": 20, "vector": "2"},
-                                                   |{"id": "3", "name": "Sloth", "personnel": 1, "vector": "4"},
-                                                   |{"id": "1", "name": "Gr€€d", "personnel": 5, "vector": "1"}
-                                                   |]""".stripMargin)
+    val expectedData = Right(Seq(
+      Foo(Some("1"), "bar"),
+      Foo(Some("2"), "baz"),
+      Foo(Some("3"), "bazaar")
+    ))
+
+    (persisterMock.loadAll[Foo](_: String)(_: Manifest[Foo])).expects("foo", *).returning(expectedData)
+
+    validateJsonResponse(app, "/foo.json", """[{"id":"1","bar":"bar"},{"id":"2","bar":"baz"},{"id":"3","bar":"bazaar"}]""")
   }
+
+  // TODO - CAS - 02/05/15 - Mock the persister in all tests
+  // TODO - CAS - 03/05/15 - Add a serialiser for each T that just writes the ID as a String. Make this configurable (some people will want the whole tree written)
   
   "TODO - Resources can reference other resources during deserialisation" in {
     pending
