651454bd8621b0379545d351dd2bb9ac020a65fe
agmenc <chris.agmen-smith@email.com>
2015-05-14 19:25:30 +0100

    Use a popular memoisation class to explore the idea in trivial-rest.

---
+++ src/main/scala/trivial/rest/caching/Memo.scala
@@ -0,0 +1,24 @@
+package trivial.rest.caching
+
+// See http://stackoverflow.com/questions/25129721/scala-memoization-how-does-this-scala-memo-work
+
+/**
+ * Generic way to create memoized functions (even recursive and multiple-arg ones)
+ *
+ * @param f the function to memoize
+ * @tparam I input to f
+ * @tparam K the keys we should use in cache instead of I
+ * @tparam O output of f
+ */
+case class Memo[I <% K, K, O](f: I => O) extends (I => O) {
+  import collection.mutable.{Map => Dict}
+  val cache = Dict.empty[K, O]
+  override def apply(x: I) = cache getOrElseUpdate (x, f(x))
+}
+
+object Memo {
+  /**
+   * Type of a simple memoized function e.g. when I = K
+   */
+  type ==>[I, O] = Memo[I, I, O]
+}
