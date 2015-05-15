16cad0d283f3b5791d88175de4669cec2e3ae046
agmenc <chris.agmen-smith@email.com>
2015-05-15 18:05:06 +0100

    Simplify JsonOnFileSystem.

--- src/main/scala/trivial/rest/caching/Memo.scala
+++ src/main/scala/trivial/rest/caching/Memo.scala
@@ -6,19 +6,15 @@ package trivial.rest.caching
  * Generic way to create memoized functions (even recursive and multiple-arg ones)
  *
  * @param f the function to memoize
- * @tparam I input to f
  * @tparam K the keys we should use in cache instead of I
  * @tparam O output of f
  */
-case class Memo[I <% K, K, O](f: I => O) extends (I => O) {
+case class Memo[K, O](f: K => O) extends (K => O) {
   import collection.mutable.{Map => Dict}
   val cache = Dict.empty[K, O]
-  override def apply(x: I) = cache getOrElseUpdate (x, f(x))
+  override def apply(x: K) = cache getOrElseUpdate (x, f(x))
 }
 
 object Memo {
-  /**
-   * Type of a simple memoized function e.g. when I = K
-   */
-  type ==>[I, O] = Memo[I, I, O]
+  def memo[K, O](f: K => O): Memo[K, O] = Memo(f)
 }
--- src/main/scala/trivial/rest/persistence/JsonOnFileSystem.scala
+++ src/main/scala/trivial/rest/persistence/JsonOnFileSystem.scala
@@ -1,9 +1,10 @@
 package trivial.rest.persistence
 
 import org.apache.commons.io.FileUtils._
+import org.json4s.Formats
 import org.json4s.native.Serialization
-import org.json4s.{Formats, MappingException}
-import trivial.rest.serialisation.{Serialiser, SerialiserExceptionHelper}
+import trivial.rest.caching.Memo._
+import trivial.rest.serialisation.Serialiser
 import trivial.rest.{Failure, Resource}
 
 import scala.reflect.io.{Directory, File}
@@ -11,22 +12,21 @@ import scala.reflect.io.{Directory, File}
 class JsonOnFileSystem(docRoot: Directory, serialiser: Serialiser) extends Persister {
 
   override def loadAll[T <: Resource[T] with AnyRef : Manifest](resourceName: String)(implicit formats: Formats): Either[Failure, Seq[T]] = {
+    // if (cached(resourceName))
+    //   cache(resourceName) else
     if (hasLocalFile(fileFor(resourceName)))
       serialiser.deserialise(fromDisk(resourceName))
     else
       Right(Seq.empty)
   }
 
-  // TODO - CAS - 01/05/15 - Require a ClassTag, so that we can fail if no class is specified, or tell the client what the class was that didn't load
   override def save[T <: Resource[T] : Manifest](resourceName: String, newItems: Seq[T])(implicit formats: Formats): Either[Failure, Int] = {
-    if (docRoot.notExists) docRoot.createDirectory()
-    val targetFile = fileFor(resourceName)
-    if (targetFile.notExists) targetFile.createFile()
+    val resourceFile = assuredFile(docRoot, resourceName)
 
     // TODO - CAS - 06/05/15 - Invalidate the cache of T
 
     loadAll[T](resourceName).right.map { previousItems =>
-      targetFile.writeAll(Serialization.write(previousItems ++ newItems))
+      resourceFile.writeAll(Serialization.write(previousItems ++ newItems))
     }
 
     Right(newItems.size)
@@ -35,22 +35,24 @@ class JsonOnFileSystem(docRoot: Directory, serialiser: Serialiser) extends Persi
   // TODO - CAS - 21/04/15 - Consider Scala async to make this write-behind: https://github.com/scala/async
   // TODO - CAS - 21/04/15 - Make this less ugly
   override def nextSequenceNumber: Int = {
-    val resourceName = "_sequence"
-    if (docRoot.notExists) docRoot.createDirectory()
-    val targetFile = fileFor(resourceName)
-    if (targetFile.notExists) {
-      targetFile.createFile()
-      targetFile.appendAll("1")
-      1
-    } else {
-      val previous = targetFile.slurp().toInt
-      val next = previous + 1
-      targetFile.writeAll(next.toString)
-      next
-    }
+    val targetFile = assuredFile(docRoot, "_sequence", "0")
+    val previous = targetFile.slurp().toInt
+    val next = previous + 1
+    targetFile.writeAll(next.toString)
+    next
   }
 
-  // TODO - CAS - 14/05/15 - Extract to a separate FileSystem dependency?
+  // TODO - CAS - 14/05/15 - Extract FS methods to a separate FileSystem dependency?
+
+  def assuredFile(docRoot: Directory, targetResourceName: String, defaultContents: String = ""): File = {
+    if (docRoot.notExists) docRoot.createDirectory()
+    val targetFile = fileFor(targetResourceName)
+    if (targetFile.notExists) {
+      targetFile.createFile()
+      targetFile.writeAll(defaultContents)
+    }
+    targetFile
+  }
 
   private def fromDisk[T <: Resource[T]](resourceName: String): String =
     readFileToString(fileFor(resourceName).jfile)
