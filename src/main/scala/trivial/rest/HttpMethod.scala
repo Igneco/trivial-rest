cb56a2bbfc1d052d23612556d40decff1af0af80
agmenc <chris.agmen-smith@email.com>
2015-06-15 13:58:12 +0100

    Move Classy to its own file

--- src/main/scala/trivial/rest/HttpMethod.scala
+++ src/main/scala/trivial/rest/HttpMethod.scala
@@ -2,8 +2,6 @@ package trivial.rest
 
 import trivial.rest.Classy._
 
-import scala.reflect.ClassTag
-
 trait HttpMethod {
   override def toString = name(this.getClass).toUpperCase
 }
@@ -21,10 +19,3 @@ object HttpMethod {
   def all: Set[HttpMethod] = Set(GetAll, Get, Put, Post, Delete)
 }
 
-// TODO - CAS - 09/06/15 - Make Classy work with Manifests as well as ClassTags.
-object Classy {
-  def apply[T : ClassTag]: String = name[T].toLowerCase
-  def runtimeClass[T : ClassTag]: Class[_] = implicitly[ClassTag[T]].runtimeClass
-  def name[T : ClassTag]: String = name(runtimeClass[T])
-  def name(clazz: Class[_]): String = clazz.getSimpleName.stripSuffix("$")
-}
